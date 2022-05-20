672cbc8d6d25c6f90e4021e818c94073aef26deb
Gianluigi Panza <84570039+Gianluis99@users.noreply.github.com>
2022-05-20 12:27:06 +0200

    update inventary

--- VRProject/Assets/Mine/Scripts/CollectibleItem.cs
+++ VRProject/Assets/Mine/Scripts/CollectibleItem.cs
@@ -6,6 +6,8 @@ public class CollectibleItem : MonoBehaviour
 {
     //nome dell'oggetto
     [SerializeField] private string itemName;
+
+    //piccola animazione per gli oggetti che si trovano nell mappa
     private bool anim;
     public float speed = 1;
 
@@ -32,16 +34,19 @@ public class CollectibleItem : MonoBehaviour
 
     private void Start()
     {
+        //avvia la coroutine che anima gli oggetti
        StartCoroutine( anima());
     }
 
     private void Update()
     {
+        //se true va verso l'alto
         if (anim)
         {
             transform.position = transform.position + new Vector3(0,speed*Time.deltaTime,0);
 
         }
+        //se false va verso il basso
         else
         {
             transform.position = transform.position - new Vector3(0, speed * Time.deltaTime, 0);
@@ -49,6 +54,7 @@ public class CollectibleItem : MonoBehaviour
         }
     }
 
+    //courotine che semplicemente cambia il valore da true a false e viceversa
     IEnumerator anima()
     {
         while (gameObject.activeSelf)
