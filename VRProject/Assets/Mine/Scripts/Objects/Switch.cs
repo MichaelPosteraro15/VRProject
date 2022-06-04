888359c0bb1f1b040967d6d95a1116cd27695581
Gianluigi Panza <84570039+Gianluis99@users.noreply.github.com>
2022-06-04 19:30:46 +0200

    added object switching

---
+++ VRProject/Assets/Mine/Scripts/Objects/Switch.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public int selectOb = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.transform.childCount);
+
+        bool change = false;
+
+        //scorriamo con la rotellina del mouse tutti gli oggetti che abbiamo a disposizione
+        if (Input.GetAxis("Mouse ScrollWheel")>0) // forward
+        {
+            //aumentiamo il contatore dell'oggetto selezionato
+            selectOb++;
+            change = true;
+
+            //se il contatore è arrivato all'ultimo oggetto disponibile ritorna al primo
+            if (selectOb == gameObject.transform.childCount)
+            {
+                selectOb = 0;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+             selectOb--;
+             change = true;
+
+            //se il contatore è arrivato all'ultimo oggetto disponibile ritorna al l'ultimo
+            if (selectOb == -1)
+            {
+                selectOb = gameObject.transform.childCount -1;
+            }
+
+        }
+
+        if (change)
+        {
+            SelectObject();
+        }
+        
+
+
+
+    }
+
+    private void SelectObject()
+    {
+        foreach (Transform ob in transform)
+        {
+            ob.gameObject.SetActive(false);
+        }
+        transform.GetChild(selectOb).gameObject.SetActive(true);
+
+    }
+}
