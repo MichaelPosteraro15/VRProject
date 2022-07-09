c7396fd898e421bafb270d6292c85995ac844084
ThickAsABrick15 <pstmhl00r15d086v@studenti.unical.it>
2022-07-09 12:14:08 +0200

    Pause menu and Start menu

---
+++ VRProject/Assets/Mine/Scripts/GameController/PauseCanvas/PauseCanvas.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseCanvas : MonoBehaviour
+{
+    [SerializeField] Popup popup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            popup.Open();
+            popup.lockMouse();
+        }
+    }
+}
