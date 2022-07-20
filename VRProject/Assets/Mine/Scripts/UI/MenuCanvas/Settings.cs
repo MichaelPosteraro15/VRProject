e9b3a3ef246883b179a7212491d9a1081e6204ef
Gianluigi Panza <84570039+Gianluis99@users.noreply.github.com>
2022-07-20 21:43:32 +0200

    update audio

---
+++ VRProject/Assets/Mine/Scripts/UI/MenuCanvas/Settings.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public void SetScreen(int fullScreen)
+    {
+        if (fullScreen == 1)
+        {
+            Screen.fullScreen = true;
+            Debug.Log("fullScreen");
+        }
+        else
+        {
+            Screen.fullScreen = false;
+            Debug.Log("minimize");
+
+        }
+
+    }
+
+    public void SetQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+        Debug.Log(quality+1);
+
+    }
+}
