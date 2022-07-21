db5f6fe56248ec2d28f5948cbd1ce69758d5ee9e
Gianluigi Panza <84570039+Gianluis99@users.noreply.github.com>
2022-07-21 10:19:02 +0200

    Update Sound.cs

--- VRProject/Assets/Mine/Scripts/Audio/Sound.cs
+++ VRProject/Assets/Mine/Scripts/Audio/Sound.cs
@@ -17,6 +17,7 @@ public class Sound {
 	public float pitch;
 
 	public bool loop = false;
+	public bool playOnAwake = false;
 
 	[HideInInspector]
 	public AudioSource source; //audioSource ci permette di riprodurre l'audioclip
